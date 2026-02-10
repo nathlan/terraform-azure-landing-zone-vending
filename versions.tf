15ba38f10287e47b2a32d80a86bf12d320e65a84
Nathan <13533617+nathanjnorris@users.noreply.github.com>
2026-02-11 09:15:01 +1000

    Add outputs and versions files

--- versions.tf
+++ versions.tf
@@ -1,14 +1,13 @@
 terraform {
   required_version = ">= 1.9"
-
   required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+      version = ">= 2.0, < 3.0"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 4.0, < 5.0"
-    }
-    azapi = {
-      source  = "azure/azapi"
-      version = ">= 2.0, < 3.0"
     }
   }
 }
